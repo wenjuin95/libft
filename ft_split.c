2a83dc79cc0263d90c16b9ba08c380a663afbab9
low <lowwenjuin27@gmail.com>
2023-10-10 13:33:34 +0800

    add ft_split

---
+++ ft_split.c
@@ -0,0 +1,82 @@
+#include "libft.h"
+
+static size_t	count_string(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		while (s[i] != c && s[i])
+			i++;
+		count++;
+	}
+	return (count);
+}
+
+static char	*string_len(char const *s, char c)
+{
+	int		len;
+	int		i;
+	char	*word;
+
+	i = 0;
+	len = count_string(s, c);
+	word = (char *)malloc(sizeof(char) * (len + 1));
+	if (word == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		word[i] = s[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**sub_string;
+	int		i;
+	int		sub_len;
+
+	sub_len = count_string(s, c);
+	i = 0;
+	sub_string = (char **)malloc(sizeof(char *) * (sub_len + 1));
+	if (sub_string == NULL)
+		return (NULL);
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s != '\0')
+		{
+			sub_string[i] = string_len(s, c);
+			i++;
+		}
+		while (*s && *s != c)
+			s++;
+	}
+	sub_string[i] = '\0';
+	return (sub_string);
+}
+
+// int main() {
+//     const char *input_string = "Hello World This Is A Test";
+//     char **result = ft_split(input_string, ' ');
+
+//     // Print the split substrings
+//     for (int i = 0; result[i] != NULL; i++) {
+//         printf("Substring %d: %s\n", i, result[i]);
+//         free(result[i]);  // Free the memory allocated for each substring
+//     }
+
+//     // Free the array of strings
+//     free(result);
+
+//     return 0;
+// }
--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
 
 /* memory */
 void	ft_bzero(void *s, size_t n);
@@ -33,6 +34,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 
 /* convert */
+char	*ft_itoa(int n);
 int		ft_atoi(char *str);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
