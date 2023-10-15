85467971b82d510b07a11fe2ed940d39e3c0bc27
wenjuin <lowwenjuin27@gmail.com>
2023-10-15 15:44:04 +0800

    chg memcmp

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -2,9 +2,9 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char		*p1;
-	const char		*p2;
-	size_t			i;
+	const unsigned char		*p1;
+	const unsigned char		*p2;
+	size_t					i;
 
 	p1 = s1;
 	p2 = s2;
