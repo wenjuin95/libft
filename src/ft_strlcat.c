efe4a9e79eef65fb93df40b2091435e4f6115acc
wenjuin <lowwenjuin27@gmail.com>
2023-10-15 16:02:40 +0800

    chg strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -3,36 +3,34 @@
 int	ft_strlcat(char *dest, char *src, size_t n)
 {
 	size_t	d_len;
-	size_t	s_len;
+	size_t	t_len;
 	size_t	i;
 
-	d_len = 0;
-	while (dest[d_len])
-		d_len++;
-	s_len = 0;
-	while (src[s_len])
-		s_len++;
-	if (d_len >= n)
-		s_len += n;
+	if (dest == 0 || src == 0)
+		return (0);
+	d_len = ft_strlen(dest);
+	if (n <= d_len)
+		return (n + ft_strlen(src));
 	else
-		s_len += d_len;
+		t_len = d_len + ft_strlen(src);
 	i = 0;
-	while (src[i] && (d_len + 1) < n)
+	while (src[i] && d_len < n - 1)
 	{
 		dest[d_len] = src[i];
 		d_len++;
 		i++;
 	}
 	dest[d_len] = '\0';
-	return (s_len);
+	return (t_len);
 }
-// #include <stdio.h>
-// #include <string.h>
-// int main()
-// {
-//     char s1[20] = "hello ";
-//     char s2[] = "world";
-//     unsigned int length = ft_strlcat(s1, s2, sizeof(s1));
-//     printf("s1:%s\n", s1);
-//     printf("total length of s1:%d\n", length);
-// }
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[20] = "hello ";
+    char s2[] = "world";
+    unsigned int length = ft_strlcat(s1, s2, sizeof(s1));
+    printf("s1:%s\n", s1);
+    printf("total length of s1:%d\n", length);
+}*/
