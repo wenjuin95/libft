0b146decf593a165049a10a2ee8574a9670630ba
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-04-20 17:29:39 +0800

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -24,7 +24,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	total_len = ft_strlen(s1) + ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (total_len + 1));
+	str = malloc(sizeof(char) * (total_len + 1));
 	if (str == NULL)
 		return (NULL);
 	while (s1[i])
