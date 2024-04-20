56fddb02abe0be4c914c65dffea4be60bfda0a73
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-04-20 15:17:49 +0800

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -15,12 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-
+	char			*str;
+	
+	str = (void *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (*(((unsigned char *)s) + i) == (unsigned char)c)
-			return (((unsigned char *)s) + i);
+		if (str[i] == (unsigned char)c) //unsigned char: store only positive values
+			return (&str[i]); //return the address of the first occurence of c
 		i++;
 	}
 	return (NULL);
