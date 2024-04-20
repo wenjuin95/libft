9698786fdfd4a1211ae1f7052c621d892bffae2a
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-04-20 13:00:51 +0800

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -14,9 +14,9 @@
 
 int	ft_isalnum(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122) || (c >= 48 && c <= 57))
+	if ((c >= 'A && c <= 'Z) || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
 	{
-		return (8);
+		return (1);
 	}
 	return (0);
 }
