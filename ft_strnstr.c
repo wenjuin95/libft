1a4799e6a52989fcdec549b631ef730ba34e3b55
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-06-11 14:50:27 +0800

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -29,7 +29,7 @@ char	*ft_strnstr(char const *haystack, char const *needle, size_t n)
 		{
 			if (needle[j + 1] == '\0')
 			{
-				return ((char *)haystack + i); //return the position of the first character of the needle
+				return ((char *)&haystack[i]); //return the position of the first character of the needle
 			}
 			j++;
 		}
